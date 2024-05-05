b0f7e14988e83eca590aa22356cb045cbd70e490
devenamulshikder <devenamulshikder@gmail.com>
2024-05-05 17:25:11 +0600

    basic project setup done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABhQ4OfpjX9M-jBOgT-iP8GVAL65WVYuU",
+  authDomain: "solosphere-16a03.firebaseapp.com",
+  projectId: "solosphere-16a03",
+  storageBucket: "solosphere-16a03.appspot.com",
+  messagingSenderId: "1012229261544",
+  appId: "1:1012229261544:web:88e40aa2ec8044d4d7e15d",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/NavBar";
+
+const Main = () => {
+  return (
+    <div>
+      {/* Navbar */}
+      <Navbar/>
+
+      {/* Outlet */}
+
+
+      {/* Footer */}
+    </div>
+  );
+};
+
+export default Main;
