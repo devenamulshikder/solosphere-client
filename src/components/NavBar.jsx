3b55142b061fe19239b68f44fd6508b9cd5fd922
devenamulshikder <devenamulshikder@gmail.com>
2024-05-05 18:04:13 +0600

    login register and footer added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,11 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import logo from '../assets/image/logo.png'
+import { AuthContext } from "../provider/AuthProvider";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const [theme, setTheme] = useState("light");
+    const {user} = useContext(AuthContext)
+  const [theme, setTheme] = useState("dark");
 
   useEffect(() => {
     localStorage.setItem("theme", theme);
@@ -11,16 +15,16 @@ const Navbar = () => {
 
   const handleToggle = (e) => {
     if (e.target.checked) {
-      setTheme("night");
-    } else {
       setTheme("light");
+    } else {
+      setTheme("dark");
     }
   };
   return (
     <div className="navbar bg-base-100 shadow-sm container px-4 mx-auto">
       <div className="flex-1">
         <div className="flex gap-2 items-center">
-          <img className="w-auto h-7" src="" alt="" />
+          <img className="w-auto h-7" src={logo} alt="" />
           <span className="font-bold">SoloSphere</span>
         </div>
       </div>
@@ -30,46 +34,52 @@ const Navbar = () => {
             <div>Home</div>
           </li>
 
-          <li>
-            <div>Login</div>
-          </li>
+          {!user && (
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+          )}
         </ul>
 
-        <div className="dropdown dropdown-end z-50">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full" title="">
-              <img
-                referrerPolicy="no-referrer"
-                alt="User Profile Photo"
-                src=""
-              />
+        {user && (
+          <div className="dropdown dropdown-end z-50">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full" title="">
+                <img
+                  referrerPolicy="no-referrer"
+                  alt="User Profile Photo"
+                  src=""
+                />
+              </div>
             </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <div className="justify-between">Add Job</div>
+              </li>
+              <li>
+                <div>My Posted Jobs</div>
+              </li>
+              <li>
+                <div>My Bids</div>
+              </li>
+              <li>
+                <div>Bid Requests</div>
+              </li>
+              <li className="mt-2">
+                <button className="bg-gray-200 block text-center">
+                  Logout
+                </button>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <div className="justify-between">Add Job</div>
-            </li>
-            <li>
-              <div>My Posted Jobs</div>
-            </li>
-            <li>
-              <div>My Bids</div>
-            </li>
-            <li>
-              <div>Bid Requests</div>
-            </li>
-            <li className="mt-2">
-              <button className="bg-gray-200 block text-center">Logout</button>
-            </li>
-          </ul>
-        </div>
+        )}
         <input
           onChange={handleToggle}
           type="checkbox"
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,15 +1,20 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 const Main = () => {
   return (
     <div>
       {/* Navbar */}
-      <Navbar/>
+      <Navbar />
 
       {/* Outlet */}
-
+      <div className="min-h-[calc(100vh-310px)]">
+        <Outlet />
+      </div>
 
       {/* Footer */}
+      <Footer />
     </div>
   );
 };
